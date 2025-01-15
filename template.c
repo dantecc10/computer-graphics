7c2acdc06ffadb5d576d445d5b46d2c9c095bae3
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-01-15 07:35:21 -0600

    created template file

---
+++ template.c
@@ -0,0 +1,26 @@
+#include <GL/glut.h>
+void inicializa(void) {
+	glClearColor(1.1, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	gluOrtho2D(0, 400, 0, 400);
+}
+
+void pinta(void){
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POINTS);
+	glVertex2i(10, 10);
+	glVertex2i(100, 100);
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char ** argv) {
+	glutInitWindowSize(400, 400);
+	glutInit(&argc, argv);
+	glutCreateWindow("Mi primer programa");
+	
+	inicializa();
+	glutDisplayFunc(pinta);
+	glutMainLoop();
+	return 0;
+}
