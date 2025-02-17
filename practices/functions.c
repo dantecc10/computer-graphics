00c5ad1b2482b14d819c8b29c8d007507fa88f87
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2025-02-16 20:03:05 -0600

    functions.c

--- practices/functions.c
+++ practices/functions.c
@@ -26,8 +26,12 @@ void graficafuncion2dSubventana(void)
     for (int i = 0; i <= cpuntos; i++)
     {
         xi[i] = ci + i * dx;
-        // yi[i] = xi[i] * sin(xi[i]);
-        yi[i] = pow((1 / 2), xi[i]);
+        // Función a evaluar
+        // yi[i] = log(xi[i]) / log(2);
+        // yi[i] = pow(xi[i], 3);
+        // yi[i] = pow(.5, xi[i]);
+        // yi[i] = cos(xi[i]);
+        // yi[i] = exp(xi[i]);
     }
 
     // Encuentra el valor mínimo y máximo de y
@@ -37,9 +41,13 @@ void graficafuncion2dSubventana(void)
     for (int i = 1; i <= cpuntos; i++)
     {
         if (yi[i] < miny)
+        {
             miny = yi[i];
+        }
         if (yi[i] > maxy)
+        {
             maxy = yi[i];
+        }
     }
 
     // Pinta los puntos aplicando la transformación
@@ -79,16 +87,3 @@ int main(int argc, char **argv)
     glutMainLoop();
     return 0;
 }
-
-// Dime los datos de entrada ideales para graficar x * sen(x)
-// 0 10 100 0 500 0 500
-
-// Esos datos me gustaron, pero sólo veo la parte derecha de la ecuación; dame los puntos para ver simétricamente la gráfica de x * sen(x)
-//  -10 10 100 0 500 0 500
-
-// Me gustaría graficar más puntos y "verla desde más lejos" para apreciar mejor la ecuación; dame los datos para graficar x * sen(x) con 1000 puntos y un rango de visión de 0 a 1000 en x y de 0 a 1000 en y
-// -10 10 1000 0 1000 0 1000
-
-//-50 50 2000 0 500 0 500
-
-// Explícame cómo afecta cada parámetro a la proyección
